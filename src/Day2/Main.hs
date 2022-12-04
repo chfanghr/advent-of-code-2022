136f9142cff9cc18d0a45cca21ef868084999ed3
Hongrui Fang <chfanghr@gmail.com>
2022-12-04 17:38:13 +0800

    add solution for day 2

---
+++ src/Day2/Main.hs
@@ -0,0 +1,82 @@
+module Main (main) where
+
+import Data.Text.IO (getContents)
+import Text.Megaparsec (Parsec, oneOf, parse, sepEndBy1)
+import Text.Megaparsec.Char (newline, space)
+
+data Shape
+  = Rock
+  | Paper
+  | Scissors
+  deriving stock (Eq)
+
+data OutCome
+  = YouLost
+  | Draw
+  | YouWon
+
+getOutCome ::
+  -- | Opponent's choice
+  Shape ->
+  -- | Your choice
+  Shape ->
+  OutCome
+getOutCome o y =
+  if o /= y
+    then
+      if secondPlayerWins o y
+        then YouWon
+        else YouLost
+    else Draw
+  where
+    secondPlayerWins Rock = (== Paper)
+    secondPlayerWins Paper = (== Scissors)
+    secondPlayerWins Scissors = (== Rock)
+
+getShapeScore :: Shape -> Natural
+getShapeScore = \case
+  Rock -> 1
+  Paper -> 2
+  Scissors -> 3
+
+getOutcomeScore :: OutCome -> Natural
+getOutcomeScore = \case
+  YouLost -> 0
+  Draw -> 3
+  YouWon -> 6
+
+type Parser = Parsec Void Text
+
+pShape :: Char -> Char -> Char -> Parser Shape
+pShape rock paper scissors =
+  oneOf [rock, paper, scissors] <&> helper
+  where
+    helper w
+      | w == rock = Rock
+      | w == paper = Paper
+      | w == scissors = Scissors
+      | otherwise = error "unreachable"
+
+pRoundScore :: Parser Natural
+pRoundScore = do
+  opponent <- pShape 'A' 'B' 'C'
+  space
+  you <- pShape 'X' 'Y' 'Z'
+
+  pure $
+    getShapeScore you
+      + getOutcomeScore (getOutCome opponent you)
+
+pTotalScore :: Parser Natural
+pTotalScore = sum <$> sepEndBy1 pRoundScore newline
+
+main :: IO ()
+main = getContents >>= runParser
+  where
+    runParser =
+      parse
+        pTotalScore
+        "<stdin>"
+        >>> either
+          (error . show)
+          print
