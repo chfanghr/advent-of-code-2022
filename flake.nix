2bca87f6df6ac4df584f326207727959f9f8361f
Hongrui Fang <chfanghr@gmail.com>
2022-12-12 14:44:02 +0800

    use ghc 924

--- flake.nix
+++ flake.nix
@@ -3,35 +3,65 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
     haskell-flake.url = "github:srid/haskell-flake";
     treefmt-flake.url = "github:srid/treefmt-flake";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit self; } {
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit self;} {
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [
         inputs.haskell-flake.flakeModule
         inputs.treefmt-flake.flakeModule
       ];
-      perSystem = { self', config, pkgs, ... }: {
+      perSystem = {
+        self',
+        config,
+        pkgs,
+        ...
+      }: {
         haskellProjects.default = {
-          root = ./.;
-          buildTools = hp: {
-            inherit (pkgs)
-              treefmt;
-          } // config.treefmt.formatters;
-          # overrides = self: super: {}
+          packages = {
+            haskell-template.root = ./.;
+          };
+          buildTools = hp: let
+            # https://github.com/NixOS/nixpkgs/issues/140774 reoccurs in GHC 9.2
+            workaround140774 = hpkg:
+              with pkgs.haskell.lib;
+                overrideCabal hpkg (drv: {
+                  enableSeparateBinOutput = false;
+                });
+          in
+            {
+              ghcid = workaround140774 hp.ghcid;
+            }
+            // config.treefmt.formatters;
           hlsCheck.enable = true;
           hlintCheck.enable = true;
+          haskellPackages = pkgs.haskell.packages.ghc92;
+          overrides = self: super: with pkgs.haskell.lib; {
+              # All these below are for GHC 9.2 compat.
+              relude = dontCheck super.relude_1_1_0_0; # Not the default in nixpkgs yet.
+              retry = dontCheck super.retry;
+              http2 = dontCheck super.http2; # Fails on darwin
+              streaming-commons = dontCheck super.streaming-commons; # Fails on darwin
+          };
         };
         treefmt.formatters = {
-          inherit (pkgs)
-            nixpkgs-fmt;
-          inherit (pkgs.haskellPackages)
+          inherit
+            (pkgs)
+            alejandra
+            ;
+          inherit
+            (pkgs.haskellPackages)
             cabal-fmt
-            fourmolu;
+            fourmolu
+            ;
         };
       };
     };
